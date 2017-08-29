ef638dd01f43dfb6c83b09d917c8c9482cb39b6d
qianchenY <yangkuanmeng@gmail.com>
2017-08-29 11:09:43 +0800

    commit of 9

---
+++ ninja-code/chapter-4/add-method.js
@@ -0,0 +1,11 @@
+function addMethod(object, name, fn) {
+  var old = object[name];                        //#1
+  object[name] = function(){
+  	console.log(fn.length, arguments.length)
+    if (fn.length == arguments.length)           //#2
+      return fn.apply(this, arguments)           //#2
+    else if (typeof old == 'function')           //#3
+      return old.apply(this, arguments);         //#3
+  };
+  
+}
