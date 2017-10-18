d8a1ff6b6426be0cd08fb8933e7c4477f966d20c
qianchenY <yangkuanmeng@gmail.com>
2017-10-18 22:00:49 +0800

    commit of 13

--- static/js/element_event.js
+++ static/js/element_event.js
@@ -1,117 +1,117 @@
 // 修复事件对象 13.3.html
 function fixEvent(event) {
 
-  function returnTrue() { return true; }
-  function returnFalse() { return false; }
+    function returnTrue() { return true; }
+    function returnFalse() { return false; }
 
-  if (!event || !event.stopPropagation) {
-    var old = event || window.event;
+    if (!event || !event.stopPropagation) {
+        var old = event || window.event;
 
-    // Clone the old object so that we can modify the values
-    event = {};
+        // Clone the old object so that we can modify the values
+        event = {};
 
-    for (var prop in old) {
-      event[prop] = old[prop];
+        for (var prop in old) {
+            event[prop] = old[prop];
+        }
+
+        // The event occurred on this element
+        if (!event.target) {
+            event.target = event.srcElement || document;
+        }
+
+        // Handle which other element the event is related to
+        event.relatedTarget = event.fromElement === event.target ?
+            event.toElement :
+            event.fromElement;
+
+        // Stop the default browser action
+        event.preventDefault = function () {
+            event.returnValue = false;
+            event.isDefaultPrevented = returnTrue;
+        };
+
+        event.isDefaultPrevented = returnFalse;
+
+        // Stop the event from bubbling
+        event.stopPropagation = function () {
+            event.cancelBubble = true;
+            event.isPropagationStopped = returnTrue;
+        };
+
+        event.isPropagationStopped = returnFalse;
+
+        // Stop the event from bubbling and executing other handlers
+        event.stopImmediatePropagation = function () {
+            this.isImmediatePropagationStopped = returnTrue;
+            this.stopPropagation();
+        };
+
+        event.isImmediatePropagationStopped = returnFalse;
+
+        // Handle mouse position
+        if (event.clientX != null) {
+            var doc = document.documentElement, body = document.body;
+
+            event.pageX = event.clientX +
+                (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+                (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+                (doc && doc.scrollTop || body && body.scrollTop || 0) -
+                (doc && doc.clientTop || body && body.clientTop || 0);
+        }
+
+        // Handle key presses
+        event.which = event.charCode || event.keyCode;
+
+        // Fix button for mouse clicks:
+        // 0 == left; 1 == middle; 2 == right
+        if (event.button != null) {
+            event.button = (event.button & 1 ? 0 :
+                (event.button & 4 ? 1 :
+                    (event.button & 2 ? 2 : 0)));
+        }
     }
 
-    // The event occurred on this element
-    if (!event.target) {
-      event.target = event.srcElement || document;
-    }
-
-    // Handle which other element the event is related to
-    event.relatedTarget = event.fromElement === event.target ?
-        event.toElement :
-        event.fromElement;
-
-    // Stop the default browser action
-    event.preventDefault = function () {
-      event.returnValue = false;
-      event.isDefaultPrevented = returnTrue;
-    };
-
-    event.isDefaultPrevented = returnFalse;
-
-    // Stop the event from bubbling
-    event.stopPropagation = function () {
-      event.cancelBubble = true;
-      event.isPropagationStopped = returnTrue;
-    };
-
-    event.isPropagationStopped = returnFalse;
-
-    // Stop the event from bubbling and executing other handlers
-    event.stopImmediatePropagation = function () {
-      this.isImmediatePropagationStopped = returnTrue;
-      this.stopPropagation();
-    };
-
-    event.isImmediatePropagationStopped = returnFalse;
-
-    // Handle mouse position
-    if (event.clientX != null) {
-      var doc = document.documentElement, body = document.body;
-
-      event.pageX = event.clientX +
-          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-          (doc && doc.clientLeft || body && body.clientLeft || 0);
-      event.pageY = event.clientY +
-          (doc && doc.scrollTop || body && body.scrollTop || 0) -
-          (doc && doc.clientTop || body && body.clientTop || 0);
-    }
-
-    // Handle key presses
-    event.which = event.charCode || event.keyCode;
-
-    // Fix button for mouse clicks:
-    // 0 == left; 1 == middle; 2 == right
-    if (event.button != null) {
-      event.button = (event.button & 1 ? 0 :
-          (event.button & 4 ? 1 :
-              (event.button & 2 ? 2 : 0)));
-    }
-  }
-
-  return event;
+    return event;
 
 }
 
 // 为元素绑定相关数据 13.4.html
-(function(){
+(function () {
     // 定义变量
-    var cache={},
-        guidCounter=1;
-        expando="date"+new Date().getTime();
+    var cache = {},
+        guidCounter = 1;
+    expando = "date" + new Date().getTime();
 
     // 存储数据
-    this.getData=function(elem){
+    this.getData = function (elem) {
         // 获取节点属性并赋值到变量上
-        var guid=elem[expando];
+        var guid = elem[expando];
         // 判断该节点属性是否有值
-        if(!guid){
+        if (!guid) {
             // 为变量、节点赋值ID
-            guid=elem[expando]=guidCounter++;
+            guid = elem[expando] = guidCounter++;
             // 根据id创建对象
-            cache[guid]={};
+            cache[guid] = {};
         };
         // 返回对象
         return cache[guid];
     };
 
     // 删除数据
-    this.removeData=function(elem){
+    this.removeData = function (elem) {
         // 获取节点属性并赋值到变量上
-        var guid=elem[expando];
+        var guid = elem[expando];
         // 判断该节点属性是否有值
-        if(!guid) return ;
+        if (!guid) return;
         // 根据id移除对象
         delete cache[guid];
         // 移除节点属性
-        try{
+        try {
             delete elem[expando];
-        }catch(e){
+        } catch (e) {
             // 报错则移除节点特性
-            if(elem.removeAttribute){
+            if (elem.removeAttribute) {
                 elem.removeAttribute(expando);
             };
         };
@@ -119,41 +119,41 @@ function fixEvent(event) {
 })();
 
 // 一个绑定事件处理程序并进行跟踪的函数 13.5.html
-(function(){
+(function () {
     var nextGuid = 1;       //运行计数器，作为独立的标识符
-    this.addEvent = function(elem, type, fn){       //接受三个参数，元素、事件类型、处理程序
+    this.addEvent = function (elem, type, fn) {       //接受三个参数，元素、事件类型、处理程序
         var data = getData(elem);       //获取与元素相关的数据，参考13.4.html
 
-        if(!data.handlers) data.handlers = {};
+        if (!data.handlers) data.handlers = {};
 
-        if(!data.handlers[type])
-        data.handlers[type] = [];
+        if (!data.handlers[type])
+            data.handlers[type] = [];
 
-        if(!fn.guid) fn.guid = nextGuid++;      //为处理程序添加独立标识符
+        if (!fn.guid) fn.guid = nextGuid++;      //为处理程序添加独立标识符
 
         data.handlers[type].push(fn);     //处理程序添加到同类型事件的数组中
 
-        if(!data.dispatcher){
+        if (!data.dispatcher) {
             data.disabled = false;
-            data.dispatcher = function(event){      //事件发生时出发绑定的函数
-                if(data.disabled) return;
+            data.dispatcher = function (event) {      //事件发生时出发绑定的函数
+                if (data.disabled) return;
                 event = fixEvent(event);        //获取修复后的事件对象 参考13.3.html
                 console.log(event);
                 var handlers = data.handlers[event.type];
-                if(handlers){
-                    for (var n=0; n<handlers.length; n++){
-                        handlers[n].call(elem,event);
+                if (handlers) {
+                    for (var n = 0; n < handlers.length; n++) {
+                        handlers[n].call(elem, event);
                     }
                 }
             }
         };
 
         // 用合适的方法，将dispatcher 方法绑定到元素上面，并只绑定一次
-        if(data.handlers[type].length == 1){
-            if(document.addEventListener){
+        if (data.handlers[type].length == 1) {
+            if (document.addEventListener) {
                 elem.addEventListener(type, data.dispatcher, false);
             }
-            else if (document.attachEvent){
+            else if (document.attachEvent) {
                 elem.attachEvent("on" + type, data.dispatcher);
             }
         }
@@ -161,10 +161,10 @@ function fixEvent(event) {
 })()
 
 // 清理处理程序 13.6.html
-function tidyUp(elem,type){
+function tidyUp(elem, type) {
 
-    function isEmpty(object){
-        for (var prop in object){
+    function isEmpty(object) {
+        for (var prop in object) {
             return false;
         };
 
@@ -173,23 +173,23 @@ function tidyUp(elem,type){
 
     var data = getData(elem);       //获取元素相关数据
 
-    if(data.handlers[type].length === 0){
+    if (data.handlers[type].length === 0) {
         delete data.handlers[type];
 
-        if(document.removeEventListener){
+        if (document.removeEventListener) {
             elem.removeEventListener(type, data.dispatcher, false);
         }
-        else if (document.detachEvent){
+        else if (document.detachEvent) {
             elem.detachEvent("on" + type, data.dispatcher);
         };
     };
 
-    if(isEmpty(data.handlers)){
+    if (isEmpty(data.handlers)) {
         delete data.handlers;
         delete data.dispatcher;
     };
 
-    if(isEmpty(data)){
+    if (isEmpty(data)) {
         removeDate(elem);
     };
 };
@@ -199,44 +199,78 @@ function tidyUp(elem,type){
 // 1、将一个元素上的所有事件绑定都删除 
 // 2、讲一个元素上的指定类型事件绑定删除
 // 3、删除一个特定处理程序实例
-(function(){
-    this.removeEvent = function(elem, type, fn){
+(function () {
+    this.removeEvent = function (elem, type, fn) {
         var data = getData(elem);
-        
-        if(!data.handlers) return;
+
+        if (!data.handlers) return;
 
         // 定义一个函数，用于删除指定类型事件
-        var removeType = function(t){
+        var removeType = function (t) {
             data.handlers[t] = [];
-            tidyUp(elem,t);     //删除指定类型事件，参考13.6.html
+            tidyUp(elem, t);     //删除指定类型事件，参考13.6.html
         };
 
         // 删除所有事件
-        if(!type){
+        if (!type) {
             for (var t in data.handlers) removeType(t);
             return;
         };
 
         var handlers = data.handlers[type];
-        if(!handlers) return;
+        if (!handlers) return;
 
         // 删除指定事件
-        if(!fn){
+        if (!fn) {
             removeType(type);
             return;
         };
 
         // 删除一个特定处理程序实例
-        if(fn.guid){
-            for(var n=0; n < handlers.length; n++){
-                if(handlers[n].guid === fn.guid){
-                    handlers.splice(n--,1);
+        if (fn.guid) {
+            for (var n = 0; n < handlers.length; n++) {
+                if (handlers[n].guid === fn.guid) {
+                    handlers.splice(n--, 1);
                 }
             }
         };
 
         // 判断该类型是否为空，是则删除该类型
-        tidyUp(elem,type);
+        tidyUp(elem, type);
 
     };
 })();
+
+// 触发事件 13.9.html
+function triggerEvent(elem, event) {
+    // 获取元素数据以及元素的父元素（用于冒泡）
+    var elemData = getData(elem),
+        parent = elem.parentNode || elem.ownerDocument;
+
+    // 如果传入的event是一个字符串，则为此创建一个event对象
+    if (typeof event === "string") {
+        event = { type: event, target: elem };
+    };
+    // 对event属性进行规范化
+    event = fixEvent(event);
+
+    // 如果传入的元素有事件调度器，则执行该类型的事件处理程序
+    if (elemData.dispatcher) {
+        elemData.dispatcher.call(elem, event);
+    };
+
+    // 除非显示停止冒泡，一直递归调用该函数，将事件向上冒泡到dom上
+    if (parent && !event.isDefaultPrevented()) {
+        triggerEvent(parent, event);
+    }   //如果dom到顶了则触发默认行为
+    else if (!parent && !event.isDefaultPrevented()) {
+        var targetData = getData(event.target);
+
+        // 判断模板元素有没有该事件的默认行为
+        if (event.target[event.type]) {
+            targetData.disabled = true;
+            event.target[event.type]();
+            targetData.disabled = false;
+        }
+    }
+}
